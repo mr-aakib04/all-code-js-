82e8bbf42330600c6e966d20eb7e0246acfa6f23
mr-aakib04 <saifiaakib291@gmail.com>
2024-10-07 22:18:36 +0530

    Create test.js
    all question

---
+++ test.js
@@ -0,0 +1,97 @@
+question no.1--------------
+// function gardecalculate(score) {
+//     if(score>=90){
+//         return "grade A";
+//     }
+//     if(score>=80){
+//         return "grade B";
+//     }
+//     if(score>=70){
+//         return "grade C";
+//     }
+//     if(score>=60){
+//         return "garde D";
+//     }
+//     if(score>=50){
+//         return "fale"
+//     }
+// }
+// let rv=gardecalculate(85);
+// console.log(rv);
+
+// question no.2------------------
+
+// let num=50;
+// let i=1;
+// while(i<=num){
+//    if(i%2!=0){
+//     console.log("odd number",i);
+//    }
+//    i++;
+// }
+
+// question no.3------------------
+
+// const numbers=[1,2,3,4,5];
+// const doubled=numbers.map(num=>num*2);
+// console.log(doubled);
+
+// question no.4-------------------
+
+// let str="Hello,world!";
+// console.log(str.includes("world"));
+
+// question no.5---------------------
+
+// function sum(num1,num2) {
+//    return num1+num2;
+   
+// }
+// let rv=sum(5,10);
+// console.log(rv);
+
+// quuestion no.6----------------------
+
+// let obj={
+//    name:"aaqib saifi",
+//    age:20,
+//    address:"delhi",
+//    fun:function p() {
+//       console.log(this.name);
+//       console.log(this.age);
+//    }
+// }
+// console.log(obj.fun());
+
+// question no.7--------------------
+
+// function Car(car1,car2) {
+//    this.car1=car1;
+//    this.car2=car2;
+// }
+// let mycar=new Car("thaar","lamborgini");
+// console.log(mycar);
+
+// // question no.8---------------------
+// function rotate(arr) {
+//    let lastdigit=arr.pop();
+//    arr.unshift(lastdigit);
+//    return arr;
+// }
+// let rv=rotate([1,2,3,4,5]);
+// console.log(rv);
+// question 9---------------
+// function rotatearray(arr) {
+//    let ld=arr.pop();
+//    arr.unshift(ld);
+//    return arr;
+// }
+// let rv=rotatearray([2,3,4,5,1]);
+// console.log(rv);
+
+// //last question---------------------
+// function string(str) {
+// return str.toUpperCase();
+// }
+// let rv=string("hello")
+// console.log(rv);
